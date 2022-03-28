45a9af4cb3db2746853441d4ad9970af20c0d89a
Firlinurannisa <firlynurannisa62@gmail.com>
2022-03-28 14:37:23 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from "react";
-import dataAlbum from "./single-sample.js";
-import Card from "./components/Card";
+import dataAlbum from "./all-sample.js";
+// import Card from "./components/Card";
+import ListPlaylist from './ListPlaylist.js';
 
-function App() {
-    return (
-        <div className="bg-light">
-            <div className="container">
-                <div className="row justify-content-center py-5">
-                    <div className="col-md-4">
-                        <Card
-                            image={dataAlbum.album.images[0].url}
-                            namaAlbum={dataAlbum.album.name}
-                            artistName={dataAlbum.artists[0].name}
-                            urlTo={dataAlbum.album.external_urls.spotify}
-                        />
-                    </div>
+class App extends React.component {
+    constructor(props) {
+        super(props);
+        this.state = {playlists:all-sample};
+    }
+
+    render() {
+        return (
+            <div classname="App">
+                <div className="header">
+                    Playlist
                 </div>
+                <ListPalylist playlists={this.state.playlists} />
             </div>
-        </div>
-    );
-}
+        );
+    };
+};
+
+
+// function App() {
+//     return (
+//         <div className="bg-light">
+//             <div className="container">
+//                 <div className="row justify-content-center py-5">
+//                     <div className="col-md-4">
+//                         <Card
+//                             image={dataAlbum.album.images[0].url}
+//                             namaAlbum={dataAlbum.album.name}
+//                             artistName={dataAlbum.artists[0].name}
+//                             urlTo={dataAlbum.album.external_urls.spotify}
+//                         />
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 export default App;
