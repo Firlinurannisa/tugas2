400cd95ac87cfa4299d0434ffa7488aa691c4076
Firlinurannisa <firlynurannisa62@gmail.com>
2022-03-28 14:31:54 +0700

    fourcommit

---
+++ src/ListPlaylist.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import playlist from 'playlist';
+
+
+const ListPlaylist = ({playlist}) =>
+(
+    <ul>
+        {playlist.map((playlist) => 
+            <playlist key={satuplaylist.image}playlist = {satuplaylist} />
+
+        
+        )}
+
+    </ul>
+)
+
+export default ListPlaylist;
